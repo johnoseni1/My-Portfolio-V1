438014c1a0efa7bcc88a5500f095d797cf574185
John Oseni <johnoseni26@gmail.com>
2022-06-11 09:08:50 +0100

    fixed it up

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
-const passport = require('passport');
 
 const app = express();
 
