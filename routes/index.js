bd293760375cdaf5846cdb96d55b40e88df23137
John Oseni <johnoseni26@gmail.com>
2022-06-11 09:16:16 +0100

    fixed it up

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// const User = require('../models/User');
-
-
-
-
-router.get('/tables', (req, res) => {
-    User.find()
-        // .then(users => res.json(users))
-        .then(users => {
-            if (users){
-             
-            res.render('tables', {
-                 users: users,
-            })
-            }
-        })
-});
-// const { ensureAuthenticated } = require('../config/checkAuth')
-
 //------------ Welcome Route ------------//
 router.get('/', (req, res) => {
     res.render('index');
@@ -45,7 +26,6 @@ router.get('/services', (req, res) => {
     res.render('services');
 });
 
-//------------ Dashboard Route ------------//
 
 
 
